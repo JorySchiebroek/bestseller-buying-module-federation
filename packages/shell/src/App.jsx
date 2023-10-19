bf1e97721903121ca32903e87a66292050c26ef9
JorySchiebroek <joryschiebroek@gmail.com>
2023-10-19 10:42:04 +0200

    Prepare Shell

--- packages/shell/src/App.jsx
+++ packages/shell/src/App.jsx
@@ -1,38 +1,90 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-const ProductsPage = React.lazy(() => import("app_products/ProductsPage"));
-const BillingPage = React.lazy(() => import("app_billing/BillingPage"));
+import { lazy } from "react";
+import { BrowserRouter, Routes, Route, Outlet, Link } from "react-router-dom";
+
+import { PageLoading } from './PageLoading/PageLoading';
+
+import "./styles.css";
+
+const CollectionsPage = lazy(() => import("app_products/ProductsPage"));
+const StylesPage = lazy(() => import("app_products/ProductsPage"));
+const PurchaseOrdersPage = lazy(() => import("app_products/ProductsPage"));
+
+const ProductsPage = lazy(() => import("app_products/ProductsPage"));
+const BillingPage = lazy(() => import("app_billing/BillingPage"));
 
 export function App() {
   return (
-    <div>
-      <Router>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Products</Link>
-            </li>
-            <li>
-              <Link to="/billing">Billing</Link>
-            </li>
-          </ul>
-        </nav>
-        <main>
-          <Switch>
-            <Route path="/billing">
-              <React.Suspense fallback="loading">
-                <BillingPage />
-              </React.Suspense>
-            </Route>
-            <Route path="/">
-              <React.Suspense fallback="loading">
-                <ProductsPage />
-              </React.Suspense>
-            </Route>
-          </Switch>
-        </main>
-      </Router>
-    </div>
+    <BrowserRouter>
+      <div style={{ height: '100vh' }}>
+        <div style={{ display: 'flex', flexDirection: 'row', height: '100%' }}>
+          <div style={{ border: '1px solid black', width: '250px' }}>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/collections">Collections</Link>
+                </li>
+                <li>
+                  <Link to="/styles">Styles</Link>
+                </li>
+                <li>
+                  <Link to="/orders">Orders</Link>
+                </li>
+                <li>
+                  <Link to="/billing">Billing</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div style={{ width: '100%', border: '1px solid black' }}>
+            <Routes>
+              <Route
+                path="/collections"
+                element={(
+                  <React.Suspense fallback={(<PageLoading />)}>
+                    <CollectionsPage />
+                  </React.Suspense>
+                )}
+              />
+              <Route
+                path="/styles"
+                element={(
+                  <React.Suspense fallback={(<PageLoading />)}>
+                    <StylesPage />
+                  </React.Suspense>
+                )}
+              />
+              <Route
+                path="/orders"
+                element={(
+                  <React.Suspense fallback={(<PageLoading />)}>
+                    <PurchaseOrdersPage />
+                  </React.Suspense>
+                )}
+              />
+
+              <Route
+                path="/billing"
+                element={(
+                  <React.Suspense fallback={(<PageLoading />)}>
+                    <BillingPage />
+                  </React.Suspense>
+                )}
+              />
+              <Route
+                path="/"
+                element={(
+                  <React.Suspense fallback={(<PageLoading />)}>
+                    <ProductsPage />
+                  </React.Suspense>
+                )}
+              />
+            </Routes>
+            <Outlet />
+          </div>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
---
+++ packages/shell/src/PageLoading/PageLoading.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const PageLoading = () => {
+  return (
+    <div
+      style={{
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <span>Loading...</span>
+    </div>
+  );
+}
